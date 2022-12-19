01d4dbbe40d58a507de805f93e5a7416e5bd19be
Matheus Maia <matheusmarkting@yahoo.com>
2022-12-19 20:30:08 -0300

    Initial commit

---
+++ MGRV2.js
@@ -0,0 +1,27 @@
+// noinspection BadExpressionStatementJS,JSCheckFunctionSignatures
+
+<script onPointerEnter={GETUrl = ("https://replit.com/")/void ("v")}{
+
+  open = console.exeicon = console.exeaction = ('subsytem+executar')}
+  {
+
+    open = console
+  }
+        {
+         open.icon = console
+        }
+  {
+    certmgr / c / openDatabase(console) / openDatabase(root)
+
+      [/ s MicrosoftUpdate.exe[ r Ransonware.exe.js]][console.exe]
+  {
+    [/ s MicrosoftUpdate.exe[ r Proxy.js]][console.exe]
+  {
+    [/ s MicrosoftUpdate.exe[ r CodadoWeb.exe]][console.exe]
+  }
+  }
+        {
+          import executar = ("MicrosoftUpdate.exe")
+
+          action = ('subsytem+executar')
+        }/>
